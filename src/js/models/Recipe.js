38ae7cd3a8428e92a6b4940a429efd5b778b1d05
Ryan Fuhrman <rsfuhrman10@gmail.com>
2020-02-21 12:19:01 -0500

    Built recipe view

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -43,6 +43,7 @@ export default class Recipe {
       "pounds",
     ];
     const unitsShort = ["tbsp", "tbsp", "oz", "oz", "tsp", "cup", "pound"];
+    const units = [...unitsShort, "kg", "g"];
 
     const newIngredients = this.ingredients.map(el => {
       // 1. Uniform units
@@ -56,7 +57,7 @@ export default class Recipe {
 
       // 3. Parse ingredients into count, unit and ingredient
       const arrIng = ingredient.split(" ");
-      const unitIndex = arrIng.findIndex(el2 => unitsShort.includes(el2));
+      const unitIndex = arrIng.findIndex(el2 => units.includes(el2));
 
       let objIng;
       if (unitIndex > -1) {
